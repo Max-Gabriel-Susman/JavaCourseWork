7724b9e2f9a0cff6f2edf831776eb328bb4c13e2
Prometheus <gabe.susman@gmail.com>
2021-03-24 21:26:09 -0600

    started assignment 4

---
+++ Assign4 - Performance Evaluation/src/EvaluationDriver.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+/**
+ * Assignment 4 for CS 1410
+ * This program evaluates the linear and binary searching, along
+ * with comparing performance difference between the selection sort
+ * and the built-in java.util.Arrays.sort.
+ *
+ */
+public class EvaluationDriver {
+    static final int MAX_VALUE = 1_000_000;
+    static final int MAX_ARRAY_SIZE = 100_000;
+    static final int ARRAY_INCREMENT = 20_000;
+    static final int NUMBER_SEARCHES = 50_000;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] generateNumbers(int howMany, int maxValue)
+    {
+
+        return new int[] {};
+    }
+
+    public static boolean linearSearch(int[] data, int search)
+    {
+
+        return true;
+    }
+
+    public static boolean binarySearch(int[] data, int search)
+    {
+
+        return true;
+    }
+
+    public static void selectionSort(int[] data)
+    {
+
+    }
+
+    public static void demoLinearSearchUnsorted()
+    {
+
+    }
+
+    public static void demoLinearSearchSorted()
+    {
+
+    }
+
+    public static void demoBinarySearchSelectionSort()
+    {
+
+    }
+
+    public static void demoBinarySearchFastSort()
+    {
+
+    }
+}
